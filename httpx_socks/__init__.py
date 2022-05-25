e981dcc857a7bd7ba041def980829f912ccf5f8e
Roman Snegirev <rsng@mail.ru>
2022-05-25 12:55:19 +0300

    Update httpx/httpcore dependencies

--- httpx_socks/__init__.py
+++ httpx_socks/__init__.py
@@ -1,5 +1,5 @@
 __title__ = 'httpx-socks'
-__version__ = '0.7.3'
+__version__ = '0.7.4'
 
 from python_socks import (
     ProxyError,
--- setup.py
+++ setup.py
@@ -37,8 +37,8 @@ setup(
     packages=['httpx_socks'],
     keywords='httpx asyncio socks socks5 socks4 http proxy',
     install_requires=[
-        'httpx>=0.21.0,<0.23.0',
-        'httpcore>=0.14.0,<0.15.0',
+        'httpx>=0.21.0,<0.24.0',
+        'httpcore>=0.14.0,<0.16.0',
         'python-socks>=2.0.0',
     ],
     extras_require={
