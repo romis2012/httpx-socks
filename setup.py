d3adf09601460618c666d99df761bbec48f8ada4
Roman Snegirev <rsng@mail.ru>
2023-10-07 13:51:31 +0300

    Refactor setup.py

--- setup.py
+++ setup.py
@@ -1,38 +1,35 @@
-import codecs
 import os
 import re
 import sys
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
-version = None
-
-with codecs.open(os.path.join(os.path.abspath(os.path.dirname(
-        __file__)), 'httpx_socks', '__init__.py'), 'r', 'latin1') as fp:
-    try:
-        version = re.findall(r"^__version__ = '([^']+)'\r?$",
-                             fp.read(), re.M)[0]
-    except IndexError:
-        raise RuntimeError('Unable to determine version.')
+from setuptools import setup
 
 if sys.version_info < (3, 6, 1):
     raise RuntimeError('httpx_socks requires Python 3.6.1+')
 
-with open('README.md') as f:
-    long_description = f.read()
+
+def get_version():
+    here = os.path.dirname(os.path.abspath(__file__))
+    filename = os.path.join(here, 'httpx_socks', '__init__.py')
+    contents = open(filename).read()
+    pattern = r"^__version__ = '(.*?)'$"
+    return re.search(pattern, contents, re.MULTILINE).group(1)
+
+
+def get_long_description():
+    with open('README.md', mode='r', encoding='utf8') as f:
+        return f.read()
+
 
 setup(
     name='httpx-socks',
     author='Roman Snegirev',
     author_email='snegiryev@gmail.com',
-    version=version,
+    version=get_version(),
     license='Apache 2',
     url='https://github.com/romis2012/httpx-socks',
     description='Proxy (HTTP, SOCKS) transports for httpx',
-    long_description=long_description,
+    long_description=get_long_description(),
     long_description_content_type='text/markdown',
     packages=['httpx_socks'],
     keywords='httpx asyncio socks socks5 socks4 http proxy',
@@ -44,5 +41,5 @@ setup(
     extras_require={
         'asyncio': ['async-timeout>=3.0.1'],
         'trio': ['trio>=0.16.0'],
-    }
+    },
 )
